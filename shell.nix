54e7b5944c8029bca636e2bbe5a3447e92b0a57d
Raghavendra Nyshadham <rn@raghnysh.com>
2021-06-12 12:26:54 +0530

    Use alectryon for literate Coq
    * shell.nix:
    
      - Add `alectryon' to the list of packages that must be installed.  I
        am hoping to write literate Coq with it.
    
      - Remove `coq' from the list because I will use OPAM to install it.
        This change is because `alectryon' depends on `coq-serapi', and
        there is no Nix expression for `coq-serapi' at present, see
        <https://github.com/ejgallego/coq-serapi/issues/241>.  As `coq' is
        installed by OPAM as a dependency of `coq-serapi', I am not
        installing `coq' from Nix to avoid conflicts and duplication with
        `coq' from OPAM.
    
    * .envrc: Initialise OPAM, and use it to install `coq-serapi' upon
      entering the top directory of the project.
    
    * .gitignore: Ignore the OPAM root directory created by OPAM in the
      top directory of the project.

--- shell.nix
+++ shell.nix
@@ -7,19 +7,25 @@ let
       url = "https://github.com/NixOS/nixpkgs/archive/4d8dd0afd2d5a35acb5e34c5c7b1674b74173d87.tar.gz";
       sha256 = "1d7z53vzq9jjy0dmq8w952dlxvajxrbmbn9926yhfp610p93ns84";
     };
+  ixnayPath = builtins.fetchTarball {
+    ## github:nyraghu/ixnay, master branch, 2021-06-11
+    url = "https://github.com/nyraghu/ixnay/archive/dc407795d7843b777c1700cc88753caa5c847087.tar.gz";
+    sha256 = "01261rkg4savsn8wcl533psqk1aiwki57jviadgrfp31w0k8pvbm";
+  };
   nixpkgs = import nixpkgsPath {};
-  coq = nixpkgs.coq;
-  dune = nixpkgs.dune_2;
-  ## This is the value of `propagatedBuildInputs' in the Nix setup of
-  ## the Coq project.
-  ## <https://github.com/coq/coq/blob/ac9a31046f82a4a489452b82c56a9d8cb7efc77c/default.nix#L74>
-  ocamlPackages = with coq.ocamlPackages; [ findlib ocaml zarith ];
-  coqPackages = [ coq dune ] ++ ocamlPackages;
-  generalPackages = [];
-  packages = coqPackages ++ generalPackages;
-  manDirs = builtins.map (x: x + "/share/man") packages;
-  MANPATH = (builtins.concatStringsSep ":" manDirs)
-            + ":" + (builtins.getEnv "MANPATH");
+  ixnay = import ixnayPath { inherit nixpkgs; };
+  ## I am using `alectryon' for literate Coq.
+  alectryon = ixnay.alectryon;
+  python = alectryon.python;
+  pythonEnvironment = python.withPackages (packages: [ alectryon ]);
+  ## I am using `opam' to install `coq-serapi' on which `alectryon'
+  ## depends.  There is no Nix expression for `coq-serapi' at present,
+  ## see <https://github.com/ejgallego/coq-serapi/issues/241>.
+  ##
+  ## `gmp' is for some OCaml packages on which `coq-serapi' depends.
+  packages = with nixpkgs; [ gmp opam ] ++ [ pythonEnvironment ];
+  packagesManPath = nixpkgs.lib.makeSearchPath "share/man" packages;
+  MANPATH = packagesManPath + ":" + (builtins.getEnv "MANPATH");
 in nixpkgs.mkShell {
   buildInputs = packages;
   inherit MANPATH;
