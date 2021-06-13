8e41b9f8cc0d53ab86b0e43bcb58231a8ed45ba6
Raghavendra Nyshadham <rn@raghnysh.com>
2021-06-13 19:22:32 +0530

    Remove alectryon
    It does not suit my way of writing Coq or mathematics at this stage.
    I will revisit this issue later.
    
    * misc/nix/environment.nix:
    * .envrc
    Remove the code necessitated by using alectryon and OPAM.  Replace it
    with code for installing Coq and related packages from Nix.
    
    * .gitignore: Remove the OPAM root directory from the list because I
      am not using OPAM now.

--- misc/nix/environment.nix
+++ misc/nix/environment.nix
@@ -6,27 +6,17 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/4d8dd0afd2d5a35acb5e34c5c7b1674b74173d87.tar.gz";
     sha256 = "1d7z53vzq9jjy0dmq8w952dlxvajxrbmbn9926yhfp610p93ns84";
   };
-  ixnayPath = builtins.fetchTarball {
-    ## github:nyraghu/ixnay, master branch, 2021-06-11
-    url = "https://github.com/nyraghu/ixnay/archive/dc407795d7843b777c1700cc88753caa5c847087.tar.gz";
-    sha256 = "01261rkg4savsn8wcl533psqk1aiwki57jviadgrfp31w0k8pvbm";
-  };
   nixpkgs = import nixpkgsPath {};
-  ixnay = import ixnayPath { inherit nixpkgs; };
-  ## I am using `alectryon' for literate Coq.
-  alectryon = ixnay.alectryon;
-  python = alectryon.python;
-  pythonEnvironment = python.withPackages (packages: [ alectryon ]);
-  generalPackages = with nixpkgs; [
-    ## I am using `opam' to install `coq-serapi' on which `alectryon'
-    ## depends.  There is no Nix expression for `coq-serapi' at
-    ## present <https://github.com/ejgallego/coq-serapi/issues/241>.
-    opam
-
-    ## For some OCaml packages on which `coq-serapi' depends.
-    gmp
+  coq = nixpkgs.coq;
+  ocamlPackages = with coq.ocamlPackages; [
+    dune_2
+    findlib
+    ocaml
+    zarith
   ];
-  packages = [ pythonEnvironment ] ++ generalPackages;
+  coqPackages = [ coq ] ++ ocamlPackages;
+  generalPackages = [];
+  packages = coqPackages ++ generalPackages;
 in {
   inherit (nixpkgs) buildEnv mkShell;
   inherit packages;
