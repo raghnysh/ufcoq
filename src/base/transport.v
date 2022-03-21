d521890659046c63d2da6be1930d7436b5caa6ad
Raghavendra Nyshadham <rn@raghnysh.com>
2022-03-21 20:25:43 +0530

    Add the code for transport on False, True, Boolean
    * src/base/transport.v: Add the code for transport on False, True,
      and Boolean.

--- src/base/transport.v
+++ src/base/transport.v
@@ -8,6 +8,7 @@
 Require Import ufcoq.base.primitive.
 Require Import ufcoq.base.construct.
 Require Import ufcoq.base.ident.
+Require Import ufcoq.base.elident.
 (* endfrag *)
 
 (* ================================================================ *)
@@ -175,6 +176,149 @@ Definition transport_relative
            ident_induction x H base.
 
 Arguments transport_relative {X F} G {x y} p e _.
+(* endfrag *)
+
+(* ================================================================ *)
+(** ** Transport on the false type                                  *)
+(* ================================================================ *)
+
+(* begfrag:e4llc30a *)
+Definition false_double_induction
+  : forall (F : False -> False -> Type) (x y : False), F x y
+  := fun (F : False -> False -> Type)
+       => false_induction
+            (fun (x : False) => forall (y : False), F x y).
+(* endfrag *)
+
+(* begfrag:535lvwe9 *)
+Definition false_double_elident
+  : forall (F : False -> False -> Type)
+           (f g : forall (x y : False), F x y)
+           (x y : False),
+      Ident (f x y) (g x y)
+  := fun (F : False -> False -> Type)
+         (f g : forall (x y : False), F x y)
+       => false_double_induction
+            (fun (x y : False) => Ident (f x y) (g x y)).
+
+Arguments false_double_elident {F} f g x y.
+(* endfrag *)
+
+(* begfrag:ahm8ghsp *)
+Definition transport_false
+  : forall (F : False -> Type) (x y : False),
+      Ident (fun (p : Ident x y) => (transport F p))
+            (false_double_induction
+               (fun (a b : False) => Ident a b -> F a -> F b)
+               x y)
+  := fun (F : False -> Type)
+       => false_double_elident
+            (@transport False F)
+            (false_double_induction
+               (fun (a b : False) => Ident a b -> F a -> F b)).
+(* endfrag *)
+
+(* ================================================================ *)
+(** ** Transport on the true type                                   *)
+(* ================================================================ *)
+
+(* begfrag:won8oy5d *)
+Definition true_double_induction
+  : forall (F : True -> True -> Type),
+      F only only -> forall (x y : True), F x y
+  := fun (F : True -> True -> Type) (u : F only only)
+       => true_induction (fun (x : True) => forall (y : True), F x y)
+                         (true_induction (fun (y : True) => F only y)
+                                         u).
+(* endfrag *)
+
+(* begfrag:xlmz2ube *)
+Definition true_double_elident
+  : forall (F : True -> True -> Type)
+           (f g : forall (x y : True), F x y),
+      Ident (f only only) (g only only)
+        -> forall (x y : True), Ident (f x y) (g x y)
+  := fun (F : True -> True -> Type)
+         (f g : forall (x y : True), F x y)
+       => true_double_induction
+            (fun (x y : True) => Ident (f x y) (g x y)).
+
+Arguments true_double_elident {F} f g _ _ _.
+(* endfrag *)
+
+(* begfrag:sdp39fus *)
+Definition transport_true
+  : forall (F : True -> Type) (x y : True),
+        Ident (fun (p : Ident x y) => transport F p)
+              (true_double_induction
+                 (fun (a b : True) => Ident a b -> F a -> F b)
+                 (fun (p : Ident only only) => transport F p)
+                 x y)
+  := fun (F : True -> Type)
+       => true_double_elident
+            (@transport True F)
+            (true_double_induction
+               (fun (a b : True) => Ident a b -> F a -> F b)
+               (fun (p : Ident only only) => transport F p))
+            (ident_unit (fun (p : Ident only only) => transport F p)).
+(* endfrag *)
+
+(* ================================================================ *)
+(** ** Transport on the boolean type                                *)
+(* ================================================================ *)
+
+(* begfrag:96eavgp6 *)
+Definition boolean_double_induction
+  : forall (F : Boolean -> Boolean -> Type),
+      F yes yes -> F yes no -> F no yes -> F no no
+        -> forall (x y : Boolean), F x y
+  := fun (F : Boolean -> Boolean -> Type)
+         (s : F yes yes) (t : F yes no) (u : F no yes) (v : F no no)
+       => boolean_induction
+            (fun (x : Boolean) => forall (y : Boolean), F x y)
+            (boolean_induction (fun (y : Boolean) => F yes y) s t)
+            (boolean_induction (fun (y : Boolean) => F no y) u v).
+(* endfrag *)
+
+(* begfrag:dnd8qln5 *)
+Definition boolean_double_elident
+  : forall (F : Boolean -> Boolean -> Type)
+           (f g : forall (x y : Boolean), F x y),
+      Ident (f yes yes) (g yes yes) -> Ident (f yes no) (g yes no)
+        -> Ident (f no yes) (g no yes) -> Ident (f no no) (g no no)
+          -> forall (x y : Boolean), Ident (f x y) (g x y)
+  := fun (F : Boolean -> Boolean -> Type)
+         (f g : forall (x y : Boolean), F x y)
+       => boolean_double_induction
+            (fun (x y : Boolean) => Ident (f x y) (g x y)).
+
+Arguments boolean_double_elident {F} f g _ _ _ _ x y.
+(* endfrag *)
+
+(* begfrag:r68r8epq *)
+Definition transport_boolean
+  : forall (F : Boolean -> Type) (x y : Boolean),
+      Ident (fun (p : Ident x y) => transport F p)
+            (boolean_double_induction
+               (fun (a b : Boolean) => Ident a b -> F a -> F b)
+               (fun (p : Ident yes yes) => transport F p)
+               (fun (p : Ident yes no) => transport F p)
+               (fun (p : Ident no yes) => transport F p)
+               (fun (p : Ident no no) => transport F p)
+               x y)
+  := fun (F : Boolean -> Type)
+       => boolean_double_elident
+            (@transport Boolean F)
+            (boolean_double_induction
+               (fun (a b : Boolean) => Ident a b -> F a -> F b)
+               (fun (p : Ident yes yes) => transport F p)
+               (fun (p : Ident yes no) => transport F p)
+               (fun (p : Ident no yes) => transport F p)
+               (fun (p : Ident no no) => transport F p))
+            (ident_unit (fun (p : Ident yes yes) => transport F p))
+            (ident_unit (fun (p : Ident yes no) => transport F p))
+            (ident_unit (fun (p : Ident no yes) => transport F p))
+            (ident_unit (fun (p : Ident no no) => transport F p)).
 (* endfrag *)
 
 (* End of file *)
