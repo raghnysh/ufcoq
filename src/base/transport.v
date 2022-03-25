072f4c28e71ad509fd048b7a85b4049a3d95bf48
Raghavendra Nyshadham <rn@raghnysh.com>
2022-03-26 00:13:27 +0530

    Add a few definitions about transport
    * src/base/transport.v: Add code on lifts of identities, transport in
      a constant family, transport in a pullback family, and the action of
      a map of families on transport.  All this content is from Section
      2.3 of the HoTT book.

--- src/base/transport.v
+++ src/base/transport.v
@@ -360,4 +360,172 @@ Definition natural_double_induction
                     (v m w)).
 (* endfrag *)
 
+(* ================================================================ *)
+(** ** The lift of an identity                                      *)
+(* ================================================================ *)
+
+(* begfrag:3p6u5byg *)
+Definition ident_lift
+  : forall (X : Type)
+           (F : X -> Type)
+           (x y : X)
+           (p : Ident x y)
+           (e : F x),
+      Ident (sigma F x e) (sigma F y (transport F p e))
+  := fun (X : Type) (F : X -> Type) (x : X)
+       =>
+         let
+           G : forall (y : X), Ident x y -> Type
+             := fun (y : X) (p : Ident x y)
+                  => forall (e : F x),
+                       Ident (sigma F x e)
+                             (sigma F y (transport F p e))
+         in let
+           base : G x (ident_unit x)
+             := fun (e : F x) => ident_unit (sigma F x e)
+         in
+           ident_induction x G base.
+
+Arguments ident_lift {X} F {x y} p e.
+(* endfrag *)
+
+(* begfrag:amyvyisd *)
+Definition ident_map_dependent
+  : forall (X : Type)
+           (F : X -> Type)
+           (f : forall (x : X), F x)
+           (x y : X)
+           (p : Ident x y),
+      Ident (transport F p (f x)) (f y)
+  := fun (X : Type) (F : X -> Type) (f : forall (x : X), F x) (x : X)
+       =>
+         let
+           G : forall (y : X), Ident x y -> Type
+             := fun (y : X) (p : Ident x y)
+                  => Ident (transport F p (f x)) (f y)
+         in let
+           base : G x (ident_unit x)
+             := ident_unit (f x)
+         in
+           ident_induction x G base.
+
+Arguments ident_map_dependent {X} F f {x y} p.
+(* endfrag *)
+
+(* ================================================================ *)
+(** ** Transport in a constant family                               *)
+(* ================================================================ *)
+
+(* begfrag:zb5p2ik1 *)
+Definition transport_constant
+  : forall (X Y : Type) (x x' : X) (p : Ident x x') (y : Y),
+      Ident (transport (@constant_function X Type Y) p y) y
+  := fun (X Y : Type) (x : X)
+       =>
+         let
+           G : forall (x' : X), Ident x x' -> Type
+             := fun (x' : X) (p : Ident x x')
+                  => forall (y : Y),
+                       Ident (transport
+                                (@constant_function X Type Y) p y)
+                             y
+         in let
+           base : G x (ident_unit x)
+             := fun (y : Y) => ident_unit y
+         in
+           ident_induction x G base.
+
+Arguments transport_constant {X Y x x'} p y.
+(* endfrag *)
+
+(* begfrag:adxkhzcz *)
+Definition ident_map_dependent_constant
+  : forall (X Y : Type) (f : X -> Y) (x x' : X) (p : Ident x x'),
+      Ident (ident_map_dependent (@constant_function X Type Y) f p)
+            (ident_compose (transport_constant p (f x))
+                           (ident_map f p))
+  := fun (X Y : Type) (f : X -> Y) (x : X)
+       =>
+         let
+           G : forall (x' : X), Ident x x' -> Type
+             := fun (x' : X) (p : Ident x x')
+                  => Ident (ident_map_dependent
+                              (@constant_function X Type Y) f p)
+                           (ident_compose (transport_constant p (f x))
+                                          (ident_map f p))
+         in let
+           base : G x (ident_unit x)
+             := ident_unit (ident_unit (f x))
+         in
+           ident_induction x G base.
+
+Arguments ident_map_dependent_constant {X Y} f {x x'} p.
+(* endfrag *)
+
+(* ================================================================ *)
+(** ** Transport in a pullback family                               *)
+(* ================================================================ *)
+
+(* begfrag:twntxsxu *)
+Definition transport_pullback
+  : forall (Y : Type)
+           (F : Y -> Type)
+           (X : Type)
+           (f : X -> Y)
+           (x x' : X)
+           (p : Ident x x')
+           (e : F (f x)),
+      Ident (transport (function_compose F f) p e)
+            (transport F (ident_map f p) e)
+  := fun (Y : Type) (F : Y -> Type) (X : Type) (f : X -> Y) (x : X)
+       =>
+         let
+           G : forall (x' : X), Ident x x' -> Type
+             := fun (x' : X) (p : Ident x x')
+                  => forall (e : F (f x)),
+                       Ident (transport (function_compose F f) p e)
+                             (transport F (ident_map f p) e)
+         in let
+           base : G x (ident_unit x)
+             := fun (e : F (f x)) => ident_unit e
+         in
+           ident_induction x G base.
+
+Arguments transport_pullback {Y} F {X} f {x x'} p e.
+(* endfrag *)
+
+(* ================================================================ *)
+(** ** Action of a map of families on transport                     *)
+(* ================================================================ *)
+
+(* begfrag:5285t6hn *)
+Definition transport_map
+  : forall (X : Type)
+           (F G : X -> Type)
+           (u : forall (x : X), F x -> G x)
+           (x y : X)
+           (p : Ident x y)
+           (e : F x),
+      Ident (transport G p (u x e))
+            (u y (transport F p e))
+  := fun (X : Type)
+         (F G : X -> Type)
+         (u : forall (x : X), F x -> G x)
+         (x : X)
+       =>
+         let
+           H : forall (y : X), Ident x y -> Type
+             := fun (y : X) (p : Ident x y)
+                  => forall (e : F x),
+                       Ident (transport G p (u x e))
+                             (u y (transport F p e))
+         in let
+           base : H x (ident_unit x)
+             := fun (e : F x) => ident_unit (u x e)
+         in
+           ident_induction x H base.
+
+Arguments transport_map {X} F G u {x y} p e.
+(* endfrag *)
+
 (* End of file *)
